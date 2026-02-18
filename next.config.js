5081128da942de7829f68631f904d38555edfc51
Dauptr <dauptr@gmail.com>
2026-02-19 00:13:48 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/space.app/:path*', // The local "build" path
+        destination: 'https://s12eh1dx2vs1-d.space.z.ai/:path*', // The target host
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
