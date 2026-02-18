6ad1e3da335e2923baa3dc0569e57c2fd0595b1b
Dauptr <dauptr@gmail.com>
2026-02-19 00:26:05 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Fixes OpenSSL issue in Node 17 if not using the flag in package.json
+  experimental: {
+    // ...
+  },
   async rewrites() {
     return [
       {
-        source: '/space.app/:path*', // The local "build" path
-        destination: 'https://s12eh1dx2vs1-d.space.z.ai/:path*', // The target host
+        source: '/space.app/:path*',
+        destination: 'https://s12eh1dx2vs1-d.space.z.ai/:path*',
       },
     ];
   },
